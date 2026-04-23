e47162d4bc785c6fb7215f875ec62587b1411e25
Allen <2351227809@qq.com>
2026-04-23 21:18:24 +0800

    交换链结束

--- VulkanTest/main.cpp
+++ VulkanTest/main.cpp
@@ -86,6 +86,9 @@ private:
 	VkQueue presentQueue;//存储呈现队列的句柄
 	VkSwapchainKHR swapChain;//交换链
 	std::vector<VkImage> swapChainImages;//交换链VkImage 的句柄，渲染操作期间引用
+	VkFormat swapChainImageFormat;//为交换链图像选择的格式和范围存储在成员变量中
+	VkExtent2D swapChainExtent;
+	std::vector<VkImageView> swapChainImageViews;
 	void initWindow() {
 		glfwInit();
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
@@ -99,6 +102,8 @@ private:
 		pickPhysicalDevice();
 		createLogicalDevice();
 		createSwapChain();
+		createImageViews();
+		createGraphicsPipeline();
 	}
 	void mainLoop() {
 		while (!glfwWindowShouldClose(window)) {
@@ -106,6 +111,9 @@ private:
 		}
 	}
 	void cleanUp() {
+		for (auto imageView : swapChainImageViews) {
+			vkDestroyImageView(device, imageView, nullptr);
+		}
 		vkDestroySwapchainKHR(device, swapChain, nullptr);
 		if (enableValidationLayers) {
 			DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
@@ -375,6 +383,36 @@ private:
 		if (vkCreateSwapchainKHR(device, &createInfo, nullptr, &swapChain) != VK_SUCCESS) {
 			throw std::runtime_error("failed to create swap chain!");
 		}
+		vkGetSwapchainImagesKHR(device, swapChain, &imageCount, nullptr);
+		swapChainImages.resize(imageCount);
+		vkGetSwapchainImagesKHR(device, swapChain, &imageCount, swapChainImages.data());
+		swapChainImageFormat = surfaceFormat.format;
+		swapChainExtent = extent;
+	}
+	void createImageViews() {//交换链图像视图创建
+		swapChainImageViews.resize(swapChainImages.size());//每个交换链图像都需要一个图像视图
+		for (size_t i = 0; i < swapChainImages.size(); i++) {//每个交换链图像都需要一个图像视图
+			VkImageViewCreateInfo createInfo{};//用于创建视图的结构体
+			createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+			createInfo.image = swapChainImages[i];
+			createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;//解释为2d贴图
+			createInfo.format = swapChainImageFormat;
+			createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;//图像的用途以及应访问图像的哪一部分
+			createInfo.subresourceRange.baseMipLevel = 0;
+			createInfo.subresourceRange.levelCount = 1;
+			createInfo.subresourceRange.baseArrayLayer = 0;
+			createInfo.subresourceRange.layerCount = 1;
+			if (vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS) {
+				throw std::runtime_error("failed to create image views!");
+			}
+		}
+	}
+	//图形管线相关
+	void createGraphicsPipeline() {
 	}
 
 	//验证层相关
