16f4f0741e9a861210d7f9e99c26c7fff93d7987
Allen <2351227809@qq.com>
2026-05-13 14:44:47 +0800

    将顶点缓冲区的数据改为从能联系cpu的临时缓冲区复制而来以提高性能；使用索引缓冲区绘制以复用顶点

--- VulkanTest/main.cpp
+++ VulkanTest/main.cpp
@@ -92,9 +92,15 @@ struct Vertex {
 	}
 };
 const std::vector<Vertex> vertices = {
-	{{0.0f, -0.5f}, {1.0f, 1.0f, 1.0f}},//{{位置}, {颜色}}，位置是二维的，颜色是三维
-	{{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
-	{{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
+	//{{位置}, {颜色}}，位置是二维的，颜色是三维
+	{{ -0.5f, -0.5f }, {1.0f, 0.0f, 0.0f}},
+	{{0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}},
+	{{0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}},
+	{{-0.5f, 0.5f}, {1.0f, 1.0f, 1.0f}}
+};
+const std::vector<uint16_t> indices = {
+	//顶点索引
+	0, 1, 2, 2, 3, 0
 };
 
 class HelloTriangleApplication {
@@ -134,6 +140,8 @@ private:
 	bool framebufferResized = false;//记录窗口大小是否发生变化
 	VkBuffer vertexBuffer;//顶点缓冲区句柄
 	VkDeviceMemory vertexBufferMemory;//内存句柄	
+	VkBuffer indexBuffer;
+	VkDeviceMemory indexBufferMemory;
 
 	void initWindow() {
 		glfwInit();
@@ -160,6 +168,7 @@ private:
 		createFramebuffers();
 		createCommandPool();
 		createVertexBuffer();
+		createIndexBuffer();
 		createCommandBuffer();
 		createSyncObjects();
 	}
@@ -183,6 +192,8 @@ private:
 	void cleanUp() {
 		cleanupSwapChain();
 
+		vkDestroyBuffer(device, indexBuffer, nullptr);
+		vkFreeMemory(device, indexBufferMemory, nullptr);
 		vkDestroyBuffer(device, vertexBuffer, nullptr);
 		vkFreeMemory(device, vertexBufferMemory, nullptr);
 
@@ -515,6 +526,113 @@ private:
 		//可选：渲染通道重新创建。仅部分情况下需要。
 	}
 
+	//缓冲区相关
+	void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory) {
+		VkBufferCreateInfo bufferInfo{};
+		bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+		bufferInfo.size = size;//缓冲区大小，单位为字节
+		bufferInfo.usage = usage;//指定目的
+		bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;//缓冲区可由某队列族独有或多个队列族共享，此处仅从图形队列访问，设为独有
+		if (vkCreateBuffer(device, &bufferInfo, nullptr, &buffer) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create vertex buffer!");
+		}
+		//分配内存//内存管理是缓冲区的重要步骤
+		VkMemoryRequirements memRequirements;//一个结构体，描述了内存需求，包括大小、字节偏移量和内存类型的适用位域
+		vkGetBufferMemoryRequirements(device, buffer, &memRequirements);//查询内存需求
+		VkMemoryAllocateInfo allocInfo{};
+		allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+		allocInfo.allocationSize = memRequirements.size;
+		allocInfo.memoryTypeIndex = findMemoryType(memRequirements.memoryTypeBits, properties);
+		if (vkAllocateMemory(device, &allocInfo, nullptr, &bufferMemory) != VK_SUCCESS) {
+			throw std::runtime_error("failed to allocate vertex buffer memory!");
+		}
+		vkBindBufferMemory(device, buffer, bufferMemory, 0);//关联申请的内存和缓冲区
+	}
+	void copyBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size) {//实现内容在缓冲区之间复制
+		//使用命令缓冲区执行复制
+		//此函数内快速完成发出命令的各阶段（类比绘制命令的发出）：命令缓冲区分配、记录、提交
+		VkCommandBufferAllocateInfo allocInfo{};
+		allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+		allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+		allocInfo.commandPool = commandPool;//用已有的命令池即可
+		allocInfo.commandBufferCount = 1;
+
+		VkCommandBuffer commandBuffer;
+		vkAllocateCommandBuffers(device, &allocInfo, &commandBuffer);
+
+		VkCommandBufferBeginInfo beginInfo{};
+		beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+		beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;//只使用一次命令缓冲区，并等待函数返回，直到复制操作完成执行
+
+		vkBeginCommandBuffer(commandBuffer, &beginInfo);
+
+		VkBufferCopy copyRegion{};//要复制的区域
+		copyRegion.srcOffset = 0; //缓冲区偏移量
+		copyRegion.dstOffset = 0; // 目标缓冲区偏移量
+		copyRegion.size = size;//缓冲区大小
+		vkCmdCopyBuffer(commandBuffer, srcBuffer, dstBuffer, 1, &copyRegion);//传输内容，指定源与目的
+		vkEndCommandBuffer(commandBuffer);
+
+		VkSubmitInfo submitInfo{};
+		submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+		submitInfo.commandBufferCount = 1;
+		submitInfo.pCommandBuffers = &commandBuffer;
+
+		vkQueueSubmit(graphicsQueue, 1, &submitInfo, VK_NULL_HANDLE);
+		vkQueueWaitIdle(graphicsQueue);//不需要等待任何东西，只等待队列空闲
+		vkFreeCommandBuffers(device, commandPool, 1, &commandBuffer);
+	}
+	//Vulkan 中的缓冲区是用于存储可由显卡读取的任意数据的内存区域。它们可以用来存储顶点数据，也可以用于许多其他目的
+	void createVertexBuffer() {//创建顶点缓冲区
+		VkDeviceSize bufferSize = sizeof(vertices[0]) * vertices.size();//计算顶点缓冲区大小
+		//使用临时和顶点两个缓冲区，原因是：最佳内存具有 VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT 标志，并且通常在专用图形卡上无法通过 CPU 访问，为使用最佳内存，我们创建一个临时缓冲区，使用它来将数据从 CPU 内存复制到 GPU 内存，以提高性能
+		VkBuffer stagingBuffer;//临时缓冲区
+		VkDeviceMemory stagingBufferMemory;
+		createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);//创建临时缓冲区，传SRC_BIT:缓冲区可以用作内存传输操作的源。
+
+		void* data;//将顶点数据复制到缓冲区
+		vkMapMemory(device, stagingBufferMemory, 0, bufferSize, 0, &data);
+		memcpy(data, vertices.data(), (size_t)bufferSize);
+		vkUnmapMemory(device, stagingBufferMemory);
+
+		createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, vertexBuffer, vertexBufferMemory);//创建真正的顶点缓冲区，传DST_BIT:缓冲区可以用作内存传输操作的目的地
+		copyBuffer(stagingBuffer, vertexBuffer, bufferSize);//数据从临时缓冲区复制到顶点缓冲区
+		vkDestroyBuffer(device, stagingBuffer, nullptr);//清理临时缓冲区
+		vkFreeMemory(device, stagingBufferMemory, nullptr);
+	}
+	uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties) {
+		//显卡可以提供不同类型的内存来分配。每种类型的内存在允许的操作和性能特征方面都有所不同。我们需要结合缓冲区的需求和我们自己的应用程序需求，找到要使用的正确内存类型
+		VkPhysicalDeviceMemoryProperties memProperties;//结构体
+		vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memProperties);
+		for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {//找到适合缓冲区的内存类型
+			if ((typeFilter & (1 << i)) && (memProperties.memoryTypes[i].propertyFlags & properties) == properties) {//typeFilter 参数将用于指定适合的内存类型的位域，遍历并检查是否将相应的位设置为 1
+				return i;
+			}
+		}
+
+		throw std::runtime_error("failed to find suitable memory type!");
+	}
+	void createIndexBuffer() {//除了缓冲区类型和大小，其余同顶点缓冲区
+		//使用索引缓冲区是因为，图像由多个三角形组成，当它们共顶点时，使用索引缓冲区可以避免重复存储顶点数据，从而节省内存并提高性能
+		VkDeviceSize bufferSize = sizeof(indices[0]) * indices.size();
+
+		VkBuffer stagingBuffer;
+		VkDeviceMemory stagingBufferMemory;
+		createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
+
+		void* data;
+		vkMapMemory(device, stagingBufferMemory, 0, bufferSize, 0, &data);
+		memcpy(data, indices.data(), (size_t)bufferSize);
+		vkUnmapMemory(device, stagingBufferMemory);
+
+		createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, indexBuffer, indexBufferMemory);
+
+		copyBuffer(stagingBuffer, indexBuffer, bufferSize);
+
+		vkDestroyBuffer(device, stagingBuffer, nullptr);
+		vkFreeMemory(device, stagingBufferMemory, nullptr);
+	}
+
 	//图形管线相关
 	static std::vector<char> readFile(const std::string& filename) {//读取shader文件，得到其二进制码
 		std::ifstream file(filename, std::ios::ate | std::ios::binary);//从末尾以二进制读取
@@ -750,44 +868,7 @@ private:
 			throw std::runtime_error("failed to create command pool!");
 		}
 	}
-	//Vulkan 中的缓冲区是用于存储可由显卡读取的任意数据的内存区域。它们可以用来存储顶点数据，也可以用于许多其他目的
-	void createVertexBuffer() {//创建顶点缓冲区
-		VkBufferCreateInfo bufferInfo{};
-		bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;//
-		bufferInfo.size = sizeof(vertices[0]) * vertices.size();//缓冲区大小，单位为字节
-		bufferInfo.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;//指定目的，用于顶点缓冲区
-		bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;//缓冲区可由某队列族独有或多个队列族共享，此处仅从图形队列访问，设为独有
-		if (vkCreateBuffer(device, &bufferInfo, nullptr, &vertexBuffer) != VK_SUCCESS) {
-			throw std::runtime_error("failed to create vertex buffer!");
-		}
-		//分配内存//内存管理是缓冲区的重要步骤
-		VkMemoryRequirements memRequirements;//一个结构体，描述了内存需求，包括大小、字节偏移量和内存类型的适用位域
-		vkGetBufferMemoryRequirements(device, vertexBuffer, &memRequirements);//查询内存需求
-		VkMemoryAllocateInfo allocInfo{};
-		allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
-		allocInfo.allocationSize = memRequirements.size;
-		allocInfo.memoryTypeIndex = findMemoryType(memRequirements.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
-		if (vkAllocateMemory(device, &allocInfo, nullptr, &vertexBufferMemory) != VK_SUCCESS) {
-			throw std::runtime_error("failed to allocate vertex buffer memory!");
-		}
-		vkBindBufferMemory(device, vertexBuffer, vertexBufferMemory, 0);//关联申请的内存和缓冲区
-		void* data;//将顶点数据复制到缓冲区
-		vkMapMemory(device, vertexBufferMemory, 0, bufferInfo.size, 0, &data);
-		memcpy(data, vertices.data(), (size_t)bufferInfo.size);
-		vkUnmapMemory(device, vertexBufferMemory);
-	}
-	uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties) {
-		//显卡可以提供不同类型的内存来分配。每种类型的内存在允许的操作和性能特征方面都有所不同。我们需要结合缓冲区的需求和我们自己的应用程序需求，找到要使用的正确内存类型
-		VkPhysicalDeviceMemoryProperties memProperties;//结构体
-		vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memProperties);
-		for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {//找到适合缓冲区的内存类型
-			if ((typeFilter & (1 << i)) && (memProperties.memoryTypes[i].propertyFlags & properties) == properties) {//typeFilter 参数将用于指定适合的内存类型的位域，遍历并检查是否将相应的位设置为 1
-				return i;
-			}
-		}
 
-		throw std::runtime_error("failed to find suitable memory type!");
-	}
 	void createCommandBuffer() {//创建命令缓冲区
 		commandBuffers.resize(MAX_FRAMES_IN_FLIGHT);
 		VkCommandBufferAllocateInfo allocInfo{};
@@ -822,9 +903,10 @@ private:
 		vkCmdBeginRenderPass(commandBuffer, &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);//启动//3rd参数：渲染通道命令将嵌入到主命令缓冲区本身中，并且不会执行辅助命令缓冲区
 		vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, graphicsPipeline);//绑定管线 //2nd参数指定管线类型，此处为图形管线
 		
-		VkBuffer vertexBuffers[] = { vertexBuffer };//顶点缓冲区绑定到点
+		VkBuffer vertexBuffers[] = { vertexBuffer };//顶点缓冲区绑定到点，可以有多个
 		VkDeviceSize offsets[] = { 0 };
 		vkCmdBindVertexBuffers(commandBuffer, 0, 1, vertexBuffers, offsets);
+		vkCmdBindIndexBuffer(commandBuffer, indexBuffer, 0, VK_INDEX_TYPE_UINT16);//绑定索引缓冲区，区别是只能有一个
 		
 		
 		//指定为动态状态的，在此处发出绘制命令之前，指定具体值。这些值设置到命令缓冲区。
@@ -842,7 +924,7 @@ private:
 		scissor.extent = swapChainExtent;
 		vkCmdSetScissor(commandBuffer, 0, 1, &scissor);
 
-		vkCmdDraw(commandBuffer, static_cast<uint32_t>(vertices.size()), 1, 0, 0);//绘制三角形命令//参数：顶点数量、实例数量、顶点偏移量、实例偏移量
+		vkCmdDrawIndexed(commandBuffer, static_cast<uint32_t>(indices.size()), 1, 0, 0, 0);//绘制三角形命令//参数：顶点数量、索引缓冲区偏移量、实例数量、顶点偏移量、实例偏移量
 		vkCmdEndRenderPass(commandBuffer);//结束渲染通道
 		if (vkEndCommandBuffer(commandBuffer) != VK_SUCCESS) {
 			throw std::runtime_error("failed to record command buffer!");
